cac51dfbeb2497e91be2009cdd0335216a8f31f1
Lee Bardon <leerbardon@gmail.com>
2019-12-26 18:26:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,12 +45,12 @@ def total_gross(source)
   # hash returned by directors_totals, and add it to a running
   # total. When done, return the grand total
 
-  result = directors_totals(source)
-  names = list_of_directors(source)
+  totals = directors_totals(source)
+  name = list_of_directors(source)
   grand_total = 0
   index = 0
-  while index < result.length do
-    grand_total += result[names[index]]
+  while index < totals.length do
+    grand_total += totals[name[index]]
     index += 1
   end
   return grand_total
